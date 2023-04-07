4e70b30f2f3ea5b5206bf99cf646c1af430bd76f
kali <kali@kali>
2023-04-07 14:44:35 -0400

    first commit

--- Downloader/views.py
+++ Downloader/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from pytube import YouTube
 from django.contrib import messages
 
 def index(request):
     if request.method== "POST":
-        link =  request.POST['link']
-        video=YouTube(link)
-        stream=video.streams.all()
-        st=list(enumerate(stream))
-        print(st)
-        data={'name':video.title,
-              'pic':video.thumbnail_url,
-              }
-        return  render(request,'index.html',{'data':data,'st':st,'link':link})
-        
+        try:
+            link =  request.POST['link']
+            video=YouTube(link)
+            stream=video.streams.filter(progressive=True,file_extension='mp4')
+            st=list(enumerate(stream))
+
+            data={'name':video.title,
+                'pic':video.thumbnail_url,
+                }
+            return  render(request,'index.html',{'data':data,'st':st,'link':link})
+        except: 
+            return HttpResponse("<center><h1> link not found go back and try check again</h1></center>")
+   
     else:
         return render(request,'index.html')
         
@@ -21,7 +24,7 @@ import os
 def download(request):
     stream=request.POST.getlist('course')
     d =  request.POST['d']
-    print(d)
+
     for i in stream:
         a=int(i)
         print(i)    
--- Youtube/settings.py
+++ Youtube/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sbv28if$9edwg*6gs)xn*gr#wg==ry4#3xgo0_hkdnv4*=mpc9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'Youtube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
